7106bb40ebeecd8901a40944f40856407590b41c
neeboo <ghostcorn@gmail.com>
2023-08-18 23:20:13 +0800

    :sparkles: added debug features

--- canisters/dapp/ego_example/src/memory.rs
+++ canisters/dapp/ego_example/src/memory.rs
@@ -1,6 +1,10 @@
-use ic_stable_structures::{memory_manager::{MemoryId, MemoryManager, VirtualMemory}, DefaultMemoryImpl, StableBTreeMap, RestrictedMemory, StableCell};
-use std::cell::{RefCell};
+use ic_stable_structures::{
+    memory_manager::{MemoryId, MemoryManager, VirtualMemory},
+    DefaultMemoryImpl, RestrictedMemory, StableBTreeMap, StableCell,
+};
+
 use crate::types::*;
+use std::cell::RefCell;
 
 const USER_PROFILE_MEM_ID: MemoryId = MemoryId::new(0);
 const USER_WALLET_MEM_ID: MemoryId = MemoryId::new(1);
@@ -8,7 +12,6 @@ const METADATA_PAGES: u64 = 16;
 
 type RM = RestrictedMemory<DefaultMemoryImpl>;
 type VM = VirtualMemory<RM>;
-
 
 thread_local! {
     pub static CONFIG: RefCell<StableCell<StableState, RM>> = RefCell::new(StableCell::init(RM::new(DefaultMemoryImpl::default(), 0..METADATA_PAGES), StableState::default()).expect("failed to initialize the config cell"));
